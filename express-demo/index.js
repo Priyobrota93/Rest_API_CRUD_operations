6f22856f012ab97448ca65bfb70466a287b38783
priyobrota <priyobrotachy@gmail.com>
2023-03-25 00:29:35 +0600

    add post validation and put request

--- express-demo/index.js
+++ express-demo/index.js
@@ -17,6 +17,17 @@ app.get(){
  */
 
 // app.put()
+/*
+Look up the course
+If not existing, return error
+
+validation
+If Invalid, return 400- Bad request
+
+update course
+Return the updated data
+*/
+
 // app.delete()
 
 /* Hello World API start */
@@ -35,16 +46,21 @@ app.get("/api/:id", (req, res) => {
 });
 
 /* Route multiple Parameters start */
-// app.get("/api/:year/:month", (req, res) => {
-//   res.send(req.params);
-// });
+/*
+ app.get("/api/:year/:month", (req, res) => {
+   res.send(req.params);
+ });
+ */
 
 /*Query Parameters */
-// app.get("/api/posts/:year/:month", (req, res) => {
-//   res.send(req.query);
-// });
+/*
+app.get("/api/posts/:year/:month", (req, res) => {
+   res.send(req.query);
+ });
+/*
 
-/*Handling GET Requests*/
+
+/*Handling GET(Read) Requests*/
 const users = [
   { id: 1, user_name: "priyo" },
   { id: 2, user_name: "Mahmud" },
@@ -56,36 +72,43 @@ app.get("/api/get/users/", (req, res) => {
 });
 app.get("/api/get/users/:id", (req, res) => {
   const user = users.find((c) => c.id === parseInt(req.params.id)); // look up the user
-  if (!user) res.status(404).send("The user with the given id was not Found"); // Not existing, return error
+  if (!user) res.status(404).send("The user with the given id was not Found"); // Not existing, return error 404
   res.send(user); //return the same user
 });
 
-/* POST request handling */
+/* POST (create) request handling */
+
 app.post("/api/post/users", (req, res) => {
   if (!req.body.name || req.body.name.length < 3) {
-    res.status(400).send("Name is required and should be minimum charecter 3.");
+    //length check
+    res.status(400).send("Name is required and should be minimum charecter 3."); // Required return 400- Bad request
     return;
   }
 
   const user = {
-    id: users.length + 1,
+    id: users.length + 1, //Id length increment
     name: req.body.name,
   };
-  users.push(user);
+  users.push(user); //Write Name
   res.send(user); //return the same user
 });
 
-/* POST request handling with inputr validation */
+/* POST(Create) request handling with inputr validation */
 app.post("/api/post/validation/users", (req, res) => {
   //input validation
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-  const result = Joi.validate(req.body, schema);
-  console.log(result);
+  /*
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(), //length check
+  });
 
-  if (!req.body.name || req.body.name.length < 3) {
-    res.status(400).send("Name is required and should be minimum charecter 3.");
+  const result = schema.validate(req.body);
+  // const result = Joi.validate(req.body, schema)
+  // console.log(result);
+  */
+  // const result = validation(req.body);
+  const { error } = validation(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message); //Required return 400- Bad request
     return;
   }
 
@@ -97,6 +120,30 @@ app.post("/api/post/validation/users", (req, res) => {
   res.send(user); //return the same user
 });
 
+//validation function
+function validation(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(), //length check
+  });
+
+  return schema.validate(user);
+}
+
+/* Put(Update) request handling */
+app.put("/api/put/users/:id", (req, res) => {
+  const user = users.find((c) => c.id === parseInt(req.params.id)); // look up the user
+  if (!user) res.status(404).send("The user with the given id was not Found"); // Not existing, return error
+
+  // const result = validation(req.body);
+  const { error } = validation(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message); //Required return 400- Bad request
+    return;
+  }
+
+  user.name = req.body.name;
+  res.send(user);
+});
 /* Delete request handling */
 app.delete("/api/delete/users/:id", (req, res) => {
   const user = users.find((c) => c.id === parseInt(req.params.id)); // look up the user
